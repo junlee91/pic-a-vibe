9d595368226b85e80730cbf7600ea2560f1ed5c5
Seho Lim <limseho657424@gmail.com>
2017-10-28 10:44:39 -0500

    added web directory to work on the front end. Added very simple routing in order to serve statics.

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-/**
- * Created by seholim on 10/28/17.
- */
-var express = require('express');
-var path = require('path');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const rootPath = path.normalize(__dirname);
+console.log(rootPath);
 const Clarifai = require('clarifai');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-
-
-/*
-
-// instantiate a new Clarifai app passing in your api key.
 const clarifai = new Clarifai.App({
     apiKey: 'ba6f72aba3a14dfdb182df5ed331c34c'
 });
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(rootPath));
 
-// predict the contents of an image by passing in a url
-clarifai.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
-    function(response) {
-        console.log(response);
-    },
-    function(err) {
-        console.error(err);
-    }
-);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/web/main.html'));
+    res.setHeader('Content-Type', 'application/json');
+    // predict the contents of an image by passing in a url
+    clarifai.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+        function(response) {
+            console.log(response);
+        },
+        function(err) {
+            console.error(err);
+        }
+    );
+});
 
-*/
+// instantiate a new Clarifai app passing in your api key.
+
+
+
+
 
 
 
 app.listen(8000);
+console.log("app listening at http://localhost:8000");
