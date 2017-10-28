150c15d35661cfd8d7a16d6b857354120071370f
Jun <jlee757@uic.edu>
2017-10-28 14:06:13 -0500

    base html code with bootstrap

--- app.js
+++ app.js
@@ -20,62 +20,59 @@ app.get('/', function(req, res) {
     res.send('/web');
 });
 
-app.get('/search/:search', function(req, res) {
-    // Replace the subscriptionKey string value with your valid subscription key.
-    var subscriptionKey = 'e8a773af38224a4599c100eb52314f48';
-    // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
-    // search APIs.  In the future, regional endpoints may be available.  If you
-    // encounter unexpected authorization errors, double-check this host against
-    // the endpoint for your Bing Web search instance in your Azure dashboard.
-    var host = 'api.cognitive.microsoft.com';
-    var bingPath = '/bing/v7.0/search';
-    var term = search;
+// Replace the subscriptionKey string value with your valid subscription key.
+var subscriptionKey = 'e8a773af38224a4599c100eb52314f48';
 
-    var response_handler = function (response) {
-        var body = '';
-        response.on('data', function (d) {
-            body += d;
-        });
-        response.on('end', function () {
-            console.log('\nRelevant Headers:\n');
-            for (var header in response.headers)
-                // header keys are lower-cased by Node.js
-                if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
-                    console.log(header + ": " + response.headers[header]);
-            body = JSON.stringify(JSON.parse(body), null, '  ');
-            console.log('\nJSON Response:\n');
-            console.log(body);
-        });
-        response.on('error', function (e) {
-            console.log('Error: ' + e.message);
-        });
+// Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+// search APIs.  In the future, regional endpoints may be available.  If you
+// encounter unexpected authorization errors, double-check this host against
+// the endpoint for your Bing Web search instance in your Azure dashboard.
+var host = 'api.cognitive.microsoft.com';
+var bingPath = '/bing/v7.0/search';
+
+var term = 'forest and river';
+
+var response_handler = function (response) {
+    var body = '';
+    response.on('data', function (d) {
+        body += d;
+    });
+    response.on('end', function () {
+        console.log('\nRelevant Headers:\n');
+        for (var header in response.headers)
+            // header keys are lower-cased by Node.js
+            if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
+                console.log(header + ": " + response.headers[header]);
+        body = JSON.stringify(JSON.parse(body), null, '  ');
+        console.log('\nJSON Response:\n');
+        console.log(body);
+    });
+    response.on('error', function (e) {
+        console.log('Error: ' + e.message);
+    });
+};
+
+var bing_web_search = function (search) {
+    console.log('Searching the Web for: ' + term);
+    var request_params = {
+        method : 'GET',
+        hostname : host,
+        path : bingPath + '?q=' + encodeURIComponent(search),
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
     };
 
-    var bing_web_search = function (search) {
-        console.log('Searching the Web for: ' + term);
-        var request_params = {
-            method : 'GET',
-            hostname : host,
-            path : bingPath + '?q=' + encodeURIComponent(search),
-            headers : {
-                'Ocp-Apim-Subscription-Key' : subscriptionKey,
-            }
-        };
+    var req = https.request(request_params, response_handler);
+    req.end();
+}
 
-        var req = https.request(request_params, response_handler);
-        req.end();
-    };
-
-    if (subscriptionKey.length === 32) {
-        bing_web_search(term);
-    } else {
-        console.log('Invalid Bing Search API subscription key!');
-        console.log('Please paste yours into the source code.');
-    }
-
-});
-
-
+if (subscriptionKey.length === 32) {
+    bing_web_search(term);
+} else {
+    console.log('Invalid Bing Search API subscription key!');
+    console.log('Please paste yours into the source code.');
+}
 
 
 //app.get('/users/:userId/books/:bookId', function (req, res) {
